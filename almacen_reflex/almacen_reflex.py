4b2e6cdbbe18822eb2c261c28452956770b11abd
franjamer <fcojavier.menayo010@gmail.com>
2024-11-24 23:09:40 +0100

    sidebar funcional pendiente de estilizar, y modulado completo de componentes

--- almacen_reflex/almacen_reflex.py
+++ almacen_reflex/almacen_reflex.py
@@ -1,41 +1,7 @@
 import reflex as rx
-from almacen_reflex.componentes.sidebar_component import sidebar
-from almacen_reflex.componentes.home_component import home_component
-from almacen_reflex.componentes.about_component import about_component
-from almacen_reflex.componentes.contact_component import contact_component
-from almacen_reflex.componentes.service_component import service_component
-from almacen_reflex.componentes.legal_component import legal_component
-from almacen_reflex.componentes.busqueda_component import busqueda_component
 from almacen_reflex.state import State
-
-# Área principal dinámica
-def main_area():
-    return rx.cond(
-        State.current_component == "home",
-        home_component(),
-        rx.cond(
-            State.current_component == "about",
-            about_component(),
-            rx.cond(
-                State.current_component == "contact",
-                contact_component(),
-                rx.cond(
-                    State.current_component == "services",
-                    service_component(),
-                    rx.cond(
-                        State.current_component == "busqueda",
-                        busqueda_component(),
-                        rx.cond(
-                            State.current_component == "legal",
-                            legal_component(),
-                            home_component()
-                        )
-                    )  
-                )
-            ),
-        ),
-    )
-
+from almacen_reflex.main_area import main_area
+from almacen_reflex.componentes.sidebar_component import sidebar
 
 # Página principal
 def main_page():
@@ -48,7 +14,6 @@ def main_page():
         ),
         direction="row",
     )
-
 
 app = rx.App()
 app.add_page(main_page, route="/")
--- almacen_reflex/main_area.py
+++ almacen_reflex/main_area.py
@@ -1,10 +1,17 @@
-from 
+import reflex as rx
+from almacen_reflex.state import State
 
-
+from almacen_reflex.componentes.home_component import home_component
+from almacen_reflex.componentes.about_component import about_component
+from almacen_reflex.componentes.contact_component import contact_component
+from almacen_reflex.componentes.service_component import service_component
+from almacen_reflex.componentes.legal_component import legal_component
+from almacen_reflex.componentes.busqueda_component import busqueda_component
+# Área principal dinámica
 def main_area():
     return rx.cond(
         State.current_component == "home",
-        home(),
+        home_component(),
         rx.cond(
             State.current_component == "about",
             about_component(),
@@ -12,9 +19,17 @@ def main_area():
                 State.current_component == "contact",
                 contact_component(),
                 rx.cond(
-                    State.current_component == "busqueda",
-                    busqueda_component(),
-                    home_component()
+                    State.current_component == "services",
+                    service_component(),
+                    rx.cond(
+                        State.current_component == "busqueda",
+                        busqueda_component(),
+                        rx.cond(
+                            State.current_component == "legal",
+                            legal_component(),
+                            home_component()
+                        )
+                    )  
                 )
             ),
         ),
